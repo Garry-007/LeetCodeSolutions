23063e62658cefb920e55317f3520add6575fdf6
Garry-007 <70472888+Garry-007@users.noreply.github.com>
2022-04-21 23:04:28 +0200

    Rename functions

--- 189_Rotate_Array/Python/unit_tests.py
+++ 189_Rotate_Array/Python/unit_tests.py
@@ -7,17 +7,17 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.instance = solution.Solution()
 
-    def rotate_wrapper(self, nums, k):
+    def rotateWrapper(self, nums, k):
         x = nums
         self.instance.rotate(x, k)
         return x
 
-    def testRotation(self):
-        self.assertEqual(self.rotate_wrapper([1, 2, 3, 4, 5, 6, 7], 3), [5, 6, 7, 1, 2, 3, 4])
-        self.assertEqual(self.rotate_wrapper([-1, -100, 3, 99], 2), [3, 99, -1, -100])
-        self.assertEqual(self.rotate_wrapper([1, 2, 3, 4], 1), [4, 1, 2, 3])
-        self.assertEqual(self.rotate_wrapper([1, 2, 3, 4, 5, 6, 7], 0), [1, 2, 3, 4, 5, 6, 7])
-        self.assertEqual(self.rotate_wrapper([1], 1), [1])
+    def testRotate(self):
+        self.assertEqual(self.rotateWrapper([1, 2, 3, 4, 5, 6, 7], 3), [5, 6, 7, 1, 2, 3, 4])
+        self.assertEqual(self.rotateWrapper([-1, -100, 3, 99], 2), [3, 99, -1, -100])
+        self.assertEqual(self.rotateWrapper([1, 2, 3, 4], 1), [4, 1, 2, 3])
+        self.assertEqual(self.rotateWrapper([1, 2, 3, 4, 5, 6, 7], 0), [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(self.rotateWrapper([1], 1), [1])
 
 
 if __name__ == '__main__':
